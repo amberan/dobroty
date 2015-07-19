7801c0e2dc7d2a7162e3ef0defcdbe72a4e5054c
amberan <amberan@gmail.com>
2015-07-19 16:31:59 +0000

    add pagination

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,8 @@
 class RecipesController < ApplicationController
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.paginate(page: params[:page], per_page: 4)
+    #@recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total - likes.thumbs_down_total}.reverse
   end
   
   def show
@@ -37,6 +38,18 @@ class RecipesController < ApplicationController
       redirect_to recipe_path(@recipe)
     else
       render :edit
+    end
+  end
+  
+  def like
+    @recipe = Recipe.find(params[:id])
+    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    if like.valid?
+      flash[:success] = "Názor byl zaznamenán"
+      redirect_to :back
+    else
+      flash[:danger] = "Vyjádřit názor na recept lze jen jednou"
+      redirect_to :back
     end
   end
   
