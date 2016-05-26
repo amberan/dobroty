16ba16d02e97b21665bf3f121201fc4d37a5edb5
amberan <amberan@gmail.com>
2016-05-26 13:25:31 +0000

    create style and ingredients, display and ui

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   def require_user
     if !logged_in?
       flash[:danger] = "Na to musíš být přihlášený."
-      redirect_to :back
+      redirect_to recipes_path
     end
   end
   
